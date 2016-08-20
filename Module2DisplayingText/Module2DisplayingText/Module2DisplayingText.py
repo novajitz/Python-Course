34a303cd5bbfafdc8d2203ff54ab201fb9ffc20c
tony <novajitz@gmail.com>
2016-08-20 17:55:56 +0100

    commit2

--- Module2DisplayingText/Module2DisplayingText/Module2DisplayingText.py
+++ Module2DisplayingText/Module2DisplayingText/Module2DisplayingText.py
@@ -1,2 +1,6 @@
 print("The capybara is the worlds largest rodent")
 print("The capybara likes to live in groups")
+
+print("the capybara lives in \nSouth America")
+print("""this is the strangest
+way to print over multiple lines""")
