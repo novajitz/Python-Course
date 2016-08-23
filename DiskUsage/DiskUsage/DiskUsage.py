aada10c7ccb1c531f990496aacd43e479927b70b
tony <novajitz@gmail.com>
2016-08-23 21:03:11 +0100

    Added logging

--- DiskUsage/DiskUsage/DiskUsage.py
+++ DiskUsage/DiskUsage/DiskUsage.py
@@ -14,8 +14,11 @@
 import sys
 import os
 import math
+import logging
+
 
 # output a useful number instead of a stupidly large byte count
+# from http://stackoverflow.com/questions/5194057/better-way-to-convert-file-sizes-in-python
 def convert_size(size):
    if (size == 0):
        return '0B'
@@ -26,29 +29,37 @@ def convert_size(size):
    return '%s %s' % (s,size_name[i])
 
 def getdiskusage(strFoldername):
-    totalsize = 0
+	totalsize = 0
 
-    for dirName, dirList, fileList in os.walk(strFoldername):
-        for fname in fileList:
-            print("\t{}".format(os.path.join(dirName, fname)))
-            totalsize = totalsize + os.path.getsize(os.path.join(dirName, fname))
-    return totalsize
+	for dirName, dirList, fileList in os.walk(strFoldername):
+		for fname in fileList:
+			#print("\t{}".format(os.path.join(dirName, fname)))
+			try:
+				totalsize += os.path.getsize(os.path.join(dirName, fname))
+			except IOError:
+				logging.info("IOError on {}".format(os.path.join(dirName, fname)))
+				print("IOError on {}".format(os.path.join(dirName, fname)))
+	return totalsize
 
 def main():
+	
+	#set up the logging
+	logging.basicConfig(filename='DiskUsage.log',format='%(asctime)s %(message)s',level=logging.DEBUG)
+	
+	# check to see if user passed a parameter
+	if len(sys.argv) >1:
+		#check passed path exists
+		if os.path.exists(sys.argv[1]):
+			os.chdir(sys.argv[1])
+		else:
+			#path not valid, so quit
+			print("Path {} not valid".format(sys.argv[1]))
+			exit()
 
-    if len(sys.argv) >1:
-        #check path exists
-        if os.path.exists(sys.argv):
-            os.chdir(sys.argv[1])
-        else:
-            #path not valid, so quit
-            print("Path {} not valid".format(sys.argv[1]))
-            exit()
+		#we gpt here so either the path is valid or we're using current dir
+		strcwd = os.getcwd()
 
-        #we gpt here so either the path is valid or we're using current dir
-        strcwd = os.getcwd()
-
-        print('Folder {} is {} bytes'.format(strcwd, convert_size(getdiskusage(strcwd))))
+		print('Folder {} is {} bytes'.format(strcwd, convert_size(getdiskusage(strcwd))))
 
 if __name__ == "__main__":
-    main()
+	main()
